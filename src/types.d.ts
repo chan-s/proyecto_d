14eea75d961f58544d734a76da136ab2d52357a6
itzelita16 <andyitzela@hotmail.com>
2019-08-18 09:49:37 -0500

    Add files via upload

---
+++ src/types.d.ts
@@ -0,0 +1,13 @@
+import { database } from "firebase";
+
+declare var $ENV: ENV;
+
+interface ENV {
+    apiKey: string,
+    authDomain: string,
+    databaseURL: string,
+    projectId: string,
+    storageBucket: string,
+    messagingSenderId: string,
+    appId: string
+}
